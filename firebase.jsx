dec510f13d2c262f88f56465ee93dae94883fa63
Om Gunturkar <141735230+om-gunturkar@users.noreply.github.com>
2025-01-23 12:27:25 +0530

    firebase.jsx

--- firebase.jsx
+++ firebase.jsx
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import{getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAWds0y7gZozQ8cCfHKM3ySSQv44Mt66BY",
-  authDomain: "loginsignup-d2c7f.firebaseapp.com",
-  projectId: "loginsignup-d2c7f",
-  storageBucket: "loginsignup-d2c7f.firebasestorage.app",
-  messagingSenderId: "600150292469",
-  appId: "1:600150292469:web:4a6771d6c536cb3b4d92bc"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
